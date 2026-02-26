0f76ec7771a4af8d1309651eb27dd377488fc5ac
Hector <jhsb1235@gmail.com>
2026-02-25 21:21:24 -0600

    salonesDisponibles actualizado

--- screens/HorarioPersonal.js
+++ screens/HorarioPersonal.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-
 import { Dimensions } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
